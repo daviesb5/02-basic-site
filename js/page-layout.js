063c8dc6816fcce6468366325b4b0b8447bedcf4
Braedon <braedondavies@gmail.com>
2019-05-25 19:24:11 -0600

    Updating files.

--- js/page-layout.js
+++ js/page-layout.js
@@ -11,7 +11,6 @@ function hamMenu(){
     }
 }
 
-/*
 //Media Query – JS
 function checkWidth(size1, size2, size3){
     if (size1.matches){
@@ -25,35 +24,3 @@ function checkWidth(size1, size2, size3){
         alert ("hashDif = " + hashDif);
     }
 }
-
-//Width Options
-var hashDif;
-var size1 = window.matchMedia("(max-width: 700px)");
-var size2 = window.matchMedia("(max-width: 950px)");
-var size3 = window.matchMedia("(min-width: 950px)");
-*/
-
-/*
-Adjusts Anchor a few pixels up.
-Credit: Eric Olson
-*/
-window.addEventListener("hashchange", function() {
-    window.scrollTo(window.scrollX, window.scrollY - 150);
-});
-
-//The function actually applying the offset
-function offsetAnchor() {
-    if(location.hash.length !== 0) {
-        window.scrollTo(window.scrollX, window.scrollY - 150);
-    }
-}
-
-// This will capture hash changes while on the page
-window.addEventListener("hashchange", offsetAnchor);
-
-// This is here so that when you enter the page with a hash,
-// it can provide the offset in that case too.  Having a timeout
-// seems necessary to allow the browser to jump to the anchor first.
-window.setTimeout(offsetAnchor, 1);
-// The delay of 1 is arbitrary and may not always work right
-// (although it did during Eric's testing).

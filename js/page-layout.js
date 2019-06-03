e5da75f03c833dbb93eb8b5454a43e1b478062a7
Braedon <braedondavies@gmail.com>
2019-06-03 08:29:39 -0600

    Updating files.

--- js/page-layout.js
+++ js/page-layout.js
@@ -31,26 +31,40 @@ Changes Background Images
 ----------------------
 Credit: the Net Ninja
 */
-var webpage = "";
-var root_img = document.querySelector(':root');
+var webpage;
 
 function setImage(webpage) {
-    if (webpage = "about") {
-        /* root_img.style.setProperty('--bg-color', 'rgba(24, 24, 24, 0.75)'); */
+    var root_img = document.querySelector(':root');
+    if (webpage == "about") {
+        alert("webpage: " + webpage);
         root_img.style.setProperty('--bg-image', 'url(https://upload.wikimedia.org/wikipedia/commons/3/3f/Germany_-_Berlin_and_Potsdam%2C_Kolonnaden_Communs_Pdm._%2828213041075%29.jpg)');
+    } else if (webpage == "credits") {
+        alert("webpage: " + webpage);
+        root_img.style.setProperty('--bg-image', 'url(https://upload.wikimedia.org/wikipedia/commons/b/be/Chalkboard_at_school.jpg)');
+    } else if (webpage == "error") {
+        alert("webpage: " + webpage);
+        root_img.style.setProperty('--bg-image', 'src=img/logos/png/solid_white_1.png');
     } else {
         alert("No image found");
     }
 }
 
 /*
-TEMPORARY SET-UP
+GIVES DETAILS
 */
 var itemImg;
 
-function giveDetails(itemImg) {
+function appDetails(itemImg) {
     if (itemImg == 'calc') {
         document.getElementById("details").innerHTML = "Knowing about my interest in programming, my brother challenged me to make a calculator.  This was my project where I tried using Javascript.  Works fine with whole numbers.  However, the 64-bit math doesn't always give correct answers for problems with decimals."
+    } else {
+        alert("Oops, something is broken...");
+    }
+}
+
+function sampleDetails(itemImg) {
+    if (itemImg == 'housing') {
+        document.getElementById("details").innerHTML = "This is a sample website for a fictional company called Zimmermann Housing.  Right now, it only has a main page with a responsive layout.  I'm planning to have a slideshow for the main page."
     } else {
         alert("Oops, something is broken...");
     }
